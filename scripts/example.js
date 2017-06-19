4c25b517a6d6681f0bd30eeec8eab0beb73c2663
ZOODUCK <zooduck@beniceoreatsomerice.com>
2017-06-19 01:35:45 +0100

    replace onclick alert with method to clear and focus the input for inputIcon @mixin examples

--- scripts/example.js
+++ scripts/example.js
@@ -2,3 +2,9 @@ const es5BabelTest = () => "example.js";
 console.log("const es5BabelTest = () => \"example.js\";");
 console.log("gulp-babel has converted the above to:");
 console.log(es5BabelTest);
+
+let resetInput = (aTag) => {
+	let inputEl = aTag.parentNode.querySelector('input');
+	inputEl.value = "";
+	inputEl.focus();
+};
